87125ffd50d9a5fb1d69c91a786d1601baf1bb0f
cesarramos95 <cesarramos.aug@gmail.com>
2021-04-23 14:27:58 -0300

    Ïmplemented pagination

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,7 +3,8 @@ import {
   View,
   Text,
   FlatList,
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator
 } from 'react-native';
 import { EnvironmentButton } from '../components/EnvironmentButton';
 import { PlantCardPrimary } from '../components/PlantCardPrimary'
@@ -40,6 +41,10 @@ export function PlantSelect() {
   const [environmentSelected, setEnvironmentSelected] = useState('all');
   const [loading, setLoading] = useState(true);
 
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
   function handleEnvironmentSelected(environment: string) {
     setEnvironmentSelected(environment);
 
@@ -51,6 +56,34 @@ export function PlantSelect() {
     );
 
     setFilteredPlants(filtered);
+  }
+
+  async function fetchPlants() {
+    const { data } = await api
+      .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+      if (!data)
+        return setLoading(true);
+
+      if (page > 1) {
+        setPlants(oldValue => [...oldValue, ...data])
+        setFilteredPlants(oldValue => [...oldValue, ...data])
+      } else {
+        setPlants(data);
+        setFilteredPlants(data);
+      }
+
+      setLoading(false);
+      setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1)
+      return;
+
+    setLoadingMore(true);
+    setPage(oldValue => oldValue + 1);
+    fetchPlants();
   }
 
   useEffect(() => {
@@ -70,14 +103,6 @@ export function PlantSelect() {
   }, []);
 
   useEffect(() => {
-    async function fetchPlants() {
-      const { data } = await api
-        .get('plants?_sort=name&_order=asc');
-        setPlants(data);
-        setFilteredPlants(data);
-        setLoading(false);
-    }
-
     fetchPlants();
   }, []);
 
@@ -121,6 +146,15 @@ export function PlantSelect() {
           )}
           showsVerticalScrollIndicator={false}
           numColumns={2}
+          onEndReachedThreshold={0.1}
+          onEndReached={({ distanceFromEnd }) => 
+            handleFetchMore(distanceFromEnd)
+          }
+          ListFooterComponent={
+            loadingMore
+            ? <ActivityIndicator color={colors.green} />
+            : <></>
+          }
         />      
       </View>
       
